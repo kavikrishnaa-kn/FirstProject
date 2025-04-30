60325f5cdf13e901f3e07e605696faae0a91aa41
kavikrishnaa-kn <knkavikrishnaa19@gmail.com>
2025-04-30 18:17:47 +0530

    j

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -3,7 +3,7 @@ import Nav from "../components/navbar.jsx";
 const About = () => {
     return(
         <>
-            <div className='h-screen w-screen bg-gray-700 '>
+            <div className='h-screen w-screen bg-gray-600 '>
                 <Nav />
             </div>
         </>
